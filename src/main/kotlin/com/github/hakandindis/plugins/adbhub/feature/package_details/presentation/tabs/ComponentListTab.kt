cc92fa6f5f25de3101909efd1f9969f4389e51b2
hakandindis <hakandindis@gmail.com>
2026-02-15 15:54:17 +0300

    Add dark-light theme support

--- src/main/kotlin/com/github/hakandindis/plugins/adbhub/feature/package_details/presentation/tabs/ComponentListTab.kt
+++ src/main/kotlin/com/github/hakandindis/plugins/adbhub/feature/package_details/presentation/tabs/ComponentListTab.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.github.hakandindis.plugins.adbhub.feature.package_details.presentation.ui.ComponentDisplay
 import com.github.hakandindis.plugins.adbhub.ui.theme.AdbHubTheme
@@ -129,7 +130,7 @@ private fun ComponentRow(display: ComponentDisplay) {
         ) {
             Text(
                 ".${display.shortName}",
-                style = AdbHubTypography.label,
+                style = AdbHubTypography.body.copy(fontWeight = FontWeight.Bold),
                 color = AdbHubTheme.colors.textMain
             )
             Text(
--- src/main/kotlin/com/github/hakandindis/plugins/adbhub/ui/theme/typography/AdbHubTypography.kt
+++ src/main/kotlin/com/github/hakandindis/plugins/adbhub/ui/theme/typography/AdbHubTypography.kt
@@ -3,7 +3,9 @@ package com.github.hakandindis.plugins.adbhub.ui.theme.typography
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.TextUnit
 import org.jetbrains.jewel.foundation.theme.JewelTheme
+import org.jetbrains.jewel.ui.typography
 
 object AdbHubTypography {
 
@@ -18,7 +20,6 @@ object AdbHubTypography {
         @Composable
         get() = JewelTheme.defaultTextStyle
 
-
     val caption: TextStyle
         @Composable
         get() = JewelTheme.defaultTextStyle.copy(
@@ -27,12 +28,57 @@ object AdbHubTypography {
 
     val label: TextStyle
         @Composable
-        get() = JewelTheme.defaultTextStyle.copy(
-            fontWeight = FontWeight.Medium,
-            fontSize = JewelTheme.defaultTextStyle.fontSize * 0.75f,
-        )
+        get() = JewelTheme.typography.labelTextStyle
 
     val console: TextStyle
         @Composable
-        get() = JewelTheme.consoleTextStyle
+        get() = JewelTheme.typography.consoleTextStyle
+
+    val labelTextStyle: TextStyle
+        @Composable
+        get() = JewelTheme.typography.labelTextStyle
+
+    val labelTextSize: TextUnit
+        @Composable
+        get() = JewelTheme.typography.labelTextSize
+
+    val h0TextStyle: TextStyle
+        @Composable
+        get() = JewelTheme.typography.h0TextStyle
+
+    val h1TextStyle: TextStyle
+        @Composable
+        get() = JewelTheme.typography.h1TextStyle
+
+    val h2TextStyle: TextStyle
+        @Composable
+        get() = JewelTheme.typography.h2TextStyle
+
+    val h3TextStyle: TextStyle
+        @Composable
+        get() = JewelTheme.typography.h3TextStyle
+
+    val h4TextStyle: TextStyle
+        @Composable
+        get() = JewelTheme.typography.h4TextStyle
+
+    val regular: TextStyle
+        @Composable
+        get() = JewelTheme.typography.regular
+
+    val medium: TextStyle
+        @Composable
+        get() = JewelTheme.typography.medium
+
+    val small: TextStyle
+        @Composable
+        get() = JewelTheme.typography.small
+
+    val editorTextStyle: TextStyle
+        @Composable
+        get() = JewelTheme.typography.editorTextStyle
+
+    val consoleTextStyle: TextStyle
+        @Composable
+        get() = JewelTheme.typography.consoleTextStyle
 }
