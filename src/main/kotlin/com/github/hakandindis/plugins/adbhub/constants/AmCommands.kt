65d5adcb14b67606a2ed3dad6a21a168a0d1cc3a
hakandindis <hakandindis@gmail.com>
2026-02-16 21:34:56 +0300

    Remove package name from deeplink execution

--- src/main/kotlin/com/github/hakandindis/plugins/adbhub/constants/AmCommands.kt
+++ src/main/kotlin/com/github/hakandindis/plugins/adbhub/constants/AmCommands.kt
@@ -8,10 +8,8 @@ object AmCommands {
     fun startActivity(componentName: String): String =
         "$SHELL_PREFIX $AM $START -n $componentName"
 
-    fun startActivityWithAction(action: String, data: String, packageName: String? = null): String {
-        val base = "$SHELL_PREFIX $AM $START -a $action -d \"$data\""
-        return if (packageName != null) "$base -p $packageName" else base
-    }
+    fun startActivityWithAction(action: String, data: String): String =
+        "$SHELL_PREFIX $AM $START -a $action -d \"$data\""
 
     fun forceStop(packageName: String): String =
         "$SHELL_PREFIX $AM force-stop $packageName"
--- src/main/kotlin/com/github/hakandindis/plugins/adbhub/feature/package_actions/presentation/PackageActionsIntent.kt
+++ src/main/kotlin/com/github/hakandindis/plugins/adbhub/feature/package_actions/presentation/PackageActionsIntent.kt
@@ -6,7 +6,7 @@ sealed class PackageActionsIntent {
     data class ClearData(val packageName: String, val deviceId: String) : PackageActionsIntent()
     data class ClearCache(val packageName: String, val deviceId: String) : PackageActionsIntent()
     data class Uninstall(val packageName: String, val deviceId: String) : PackageActionsIntent()
-    data class LaunchDeepLink(val uri: String, val packageName: String, val deviceId: String) : PackageActionsIntent()
+    data class LaunchDeepLink(val uri: String, val deviceId: String) : PackageActionsIntent()
     data class StayAwake(val enabled: Boolean, val deviceId: String) : PackageActionsIntent()
     data class PackageEnabled(val packageName: String, val enabled: Boolean, val deviceId: String) :
         PackageActionsIntent()
